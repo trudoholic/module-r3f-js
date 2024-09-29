2bad911277dcf07c072faab70dc932f4cf98e8b1
Oleg <35769958+trudoholic@users.noreply.github.com>
2024-09-29 14:09:47 +0300

    dividers

--- src/components/ModuleV1.jsx
+++ src/components/ModuleV1.jsx
@@ -1,14 +1,30 @@
+import {useMemo} from "react"
 import {FrontalBox} from "./FrontalBox.jsx"
 import {HorizontalBox} from "./HorizontalBox.jsx"
 import {ProfileBox} from "./ProfileBox.jsx"
 
-const
+const // carcass
+  moduleWidth_ = 1100,
   moduleWidth = 1100 / 1000,
   moduleHeight = 680 / 1000,
   moduleDepth = 450 / 1000,
-  panelThickness = 16 / 1000
+  panelThickness_ = 16,
+  panelThickness = 16 / 1000,
+  split = 3
 
 export function ModuleV1() {// props
+
+  const dividers = useMemo(() => {
+    const n = split - 1
+    const partitionWidth = Math.floor((moduleWidth_ - panelThickness_ * (n + 2)) / split)
+    const remainingWidth = moduleWidth_ - partitionWidth * n
+    const list = [...Array(n).keys()].map((i) => ({
+      id: `Divider${i}`,
+      x: (partitionWidth + panelThickness_) * (i + 1),
+    }))
+    return { list, partitionWidth, remainingWidth }
+  }, [moduleWidth, split])
+  console.log(dividers)
 
   return (
     <group
@@ -47,11 +63,11 @@ export function ModuleV1() {// props
       />
 
       {
-        [...Array(3).keys()].map((i) => (
+        dividers.list.map(divider => (
           <ProfileBox
-            key={i}
-            name={`Divider${i}`}
-            position={{x: .2 * i, y: panelThickness, z: 0}}
+            key={divider.id}
+            name={divider.id}
+            position={{x: divider.x / 1000, y: panelThickness, z: 0}}
             size={{x: moduleDepth, y: moduleHeight - 2 * panelThickness, z: panelThickness}}
           />
         ))
